6927dcbeb46c3f1a43368f092e350a1e370bef31
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-18 18:56:15 -0300

    add filter to view duration time

---
+++ src/filter/TimeFilter.java
@@ -0,0 +1,54 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class TimeFilter
+ */
+@WebFilter("/*")
+public class TimeFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public TimeFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		long tempoInicial = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long tempoFinal = System.currentTimeMillis();
+		
+		System.out.println("tempo: " + (tempoFinal - tempoInicial));
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
